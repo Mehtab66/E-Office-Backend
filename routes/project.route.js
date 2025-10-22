704c52d44af62a7050accb5657879b7532985c9f
haiderarshad021 <haiderarshad021@gmail.com>
2025-10-22 14:14:21 +0500

    fixed both frontend and backend of projects edit feature for projects and add delevireable

--- routes/project.route.js
+++ routes/project.route.js
@@ -101,7 +101,6 @@ const taskRouter = require("./task.route");
 
 // 2. IMPORT YOUR DELIVERABLE ROUTER (THIS WAS MISSING)
 const deliverableRouter = require("./deliverable.route"); // <-- ADD THIS
-
 const router = express.Router();
 
 // --- /api/projects/ ---
